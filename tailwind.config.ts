63f575d621915d8d7233f62c8592b3ffc172f469
Bernardo Knoblauch <bekno.adm@gmail.com>
2025-03-23 12:18:59 +0100

    fix: update styles configuration for production

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,7 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -22,9 +20,15 @@ const config: Config = {
           900: '#0c4a6e',
         },
       },
+      aspectRatio: {
+        'w-1': '1',
+        'h-1': '1',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
 
 export default config 
